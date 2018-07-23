a5e8eda693d5a45bd41558bada031d059346e600
ti-ginkgo <t_6_ginkgonut_18_i@yahoo.co.jp>
2018-07-24 01:35:07 +0900

    change fcn.py

--- ComputerVision/FCN/fcn.py
+++ ComputerVision/FCN/fcn.py
@@ -22,7 +22,7 @@ def build(inputs, labels, fcn_classes):
 
     conv1_1 = my_conv2d_layer(inputs)
     conv1_2 = my_conv2d_layer(conv1_1)
-    max_pool1 = my_max_pooling2d_layer(conv1_2)
+    pool1 = my_max_pooling2d_layer(conv1_2)
 
     conv2_1 = my_conv2d_layer(max_pool1, filters=128)
     conv2_2 = my_conv2d_layer(conv2_1, filters=128)
@@ -30,22 +30,28 @@ def build(inputs, labels, fcn_classes):
 
     conv3_1 = my_conv2d_layer(max_pool2, filters=256)
     conv3_2 = my_conv2d_layer(conv3_1, filters=256)
-    conv3_3 = my_conv2d_layer(conv3_1, filters=256)
-    max_pool3 = my_max_pooling2d_layer(conv3_3)
+    conv3_3 = my_conv2d_layer(conv3_2, filters=256)
+    pool3 = my_max_pooling2d_layer(conv3_3)
 
     conv4_1 = my_conv2d_layer(max_pool3, filters=512)
     conv4_2 = my_conv2d_layer(conv4_1, filters=512)
-    conv4_3 = my_conv2d_layer(conv4_3, filters=512)
-    max_pool4 = my_max_pooling2d_layer(conv4_2)
+    conv4_3 = my_conv2d_layer(conv4_2, filters=512)
+    pool4 = my_max_pooling2d_layer(conv4_2)
 
     conv5_1 = my_conv2d_layer(max_pool4, filters=512)
     conv5_2 = my_conv2d_layer(conv5_1, filters=512)
     conv5_3 = my_conv2d_layer(conv5_2, filters=512)
-    max_pool5 = my_max_pooling2d_layer(conv5_3)
+    pool5 = my_max_pooling2d_layer(conv5_3)
 
-    pool3 = my_conv2d_layer(max_pool3, filters=fcn_classes, kernel_size=1, padding='valid')
-    pool4 = my_conv2d_layer(max_pool4, filters=fcn_classes, kernel_size=1, padding='valid')
-    pool5 = my_conv2d_layer(max_pool5, filters=fcn_classes, kernel_size=1, padding='valid')
+
+
+
+
+
+
+    pool3 = my_conv2d_layer(pool3, filters=fcn_classes, kernel_size=1, padding='valid')
+    pool4 = my_conv2d_layer(pool4, filters=fcn_classes, kernel_size=1, padding='valid')
+    pool5 = my_conv2d_layer(pool5, filters=fcn_classes, kernel_size=1, padding='valid')
 
     deconv4 = my_deconv2d_layer(pool4, filters=fcn_classes, kernel_size=4, strides=2)
     deconv4 = tf.keras.layers.Cropping2D(deconv4, (2, 2), (2, 2))
@@ -58,4 +64,4 @@ def build(inputs, labels, fcn_classes):
     deconv = tf.keras.layers.Cropping2D(deconv, (4, 4), (4, 4))
 
     output = tf.reshape(deconv, fcn_classes, 224*224)
-    loss = tf.nn.softmax_cross_entropy_with_logits(labels=labels, logits=output, dim=fcn_classes)
+    loss = tf.nn.softmax_cross_entropy_with_logits_v2(labels=labels, logits=output, dim=fcn_classes)
