cc1b4ae1347f95215ee0dc92850788edc900aeb0
ti-ginkgo <t_6_ginkgonut_18_i@yahoo.co.jp>
2018-07-29 02:52:01 +0900

    update

--- MNIST/dnn.py
+++ MNIST/dnn.py
@@ -42,7 +42,7 @@ def main():
     with tf.Session() as sess:
         init.run()
         for epoch in range(n_epochs):
-            for iteration in range(mnist.train.num_examples // batch_size):
+            for _ in range(mnist.train.num_examples // batch_size):
                 X_batch, y_batch = mnist.train.next_batch(batch_size)
                 sess.run(training_op, feed_dict={X: X_batch, y: y_batch})
             acc_train = accuracy.eval(feed_dict={X: X_batch, y: y_batch})
