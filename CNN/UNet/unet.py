f51614e64c3369bb050163fc0d6ef3a66bb3bbbf
ti-ginkgo <t_6_ginkgonut_18_i@yahoo.co.jp>
2018-07-30 01:33:25 +0900

    add comment

--- CNN/UNet/unet.py
+++ CNN/UNet/unet.py
@@ -50,28 +50,28 @@ class UNet:
 
         ## layer6
         up6 = UpSampling2D(size=(2, 2))(drop5)
-        conv6_1 = my_Conv2D(filters=512, kernel_size=2)(up6)
+        conv6_1 = my_Conv2D(filters=512, kernel_size=2)(up6)  # crop by kernel_size
         merge6 = Concatenate(axis=3)([drop4, conv6_1])
         conv6_2 = my_Conv2D(filters=512)(merge6)
         conv6_3 = my_Conv2D(filters=512)(conv6_2)
 
         ## layer7
         up7 = UpSampling2D(size=(2, 2))(conv6_3)
-        conv7_1 = my_Conv2D(filters=256, kernel_size=2)(up7)
+        conv7_1 = my_Conv2D(filters=256, kernel_size=2)(up7)  # crop by kernel_size
         merge7 = Concatenate(axis=3)([conv3_2, conv7_1])
         conv7_2 = my_Conv2D(filters=256)(merge7)
         conv7_3 = my_Conv2D(filters=256)(conv7_2)
 
         ## layer8
         up8 = UpSampling2D(size=(2, 2))(conv7_3)
-        conv8_1 = my_Conv2D(filters=128, kernel_size=2)(up8)
+        conv8_1 = my_Conv2D(filters=128, kernel_size=2)(up8) # crop by kernel_size
         merge8 = Concatenate(axis=3)([conv2_2, conv8_1])
         conv8_2 = my_Conv2D(filters=128)(merge8)
         conv8_3 = my_Conv2D(filters=128)(conv8_2)
 
         ## layer9
         up9 = UpSampling2D(size=(2, 2))(conv8_3)
-        conv9_1 = my_Conv2D(filters=64, kernel_size=2)(up9)
+        conv9_1 = my_Conv2D(filters=64, kernel_size=2)(up9) # crop by kernel_size
         merge9 = Concatenate(axis=3)([conv1_2, conv9_1])
         conv9_2 = my_Conv2D(filters=64)(merge9)
         conv9_3 = my_Conv2D(filters=64)(conv9_2)
