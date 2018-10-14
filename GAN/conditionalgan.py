06be98df254c42217030818cea06cce9c40520f4
ti-ginkgo <t_6_ginkgonut_18_i@yahoo.co.jp>
2018-10-14 16:19:45 +0900

    change conditional gan

--- GAN/conditionalgan.py
+++ GAN/conditionalgan.py
@@ -9,28 +9,28 @@ from keras.datasets import mnist
 from keras.preprocessing import image
 
 
-latent_dim = 28
+latent_dim = 100
 height = 28
 width = 28
 channels = 1
 CLASS_NUM = 10
 
 def build_generator():
-    generator_input = keras.Input(shape=(latent_dim, CLASS_NUM))
+    generator_input = keras.Input(shape=(latent_dim + CLASS_NUM,))
 
     x = layers.Dense(1024)(generator_input)
     x = layers.BatchNormalization()(x)
     x = layers.ReLU()(x)
-    x = layers.Dense(7 * 7 * 128)
+    x = layers.Dense(7 * 7 * 128)(x)
     x = layers.BatchNormalization()(x)
     x = layers.Activation('relu')(x)
     x = layers.Reshape((7, 7, 128))(x)
-    x = layers.UpSampling2D(2, 2)(x)
-    x = layers.Conv2D(64, 5, 5, padding='same')
-    x = layers.BatchNormalization()
+    x = layers.UpSampling2D((2, 2))(x)
+    x = layers.Conv2D(64, 5, padding='same')(x)
+    x = layers.BatchNormalization()(x)
     x = layers.Activation('relu')(x)
-    x = layers.UpSampling2D(2, 2)(x)
-    x = layers.Conv2D(1, 5, 5, padding='same')
+    x = layers.UpSampling2D((2, 2))(x)
+    x = layers.Conv2D(1, 5, padding='same')(x)
     x = layers.Activation('tanh')(x)
     
     generator = keras.models.Model(generator_input, x)
@@ -42,14 +42,14 @@ def build_generator():
 def build_discriminator():
     discriminator_input = layers.Input(shape=(height, width, channels+CLASS_NUM))
 
-    x = layers.Conv2D(64, 5, 5, stides=2, padding='same')(discriminator_input)
+    x = layers.Conv2D(64, 5, padding='same')(discriminator_input)
     x = layers.LeakyReLU(0.2)(x)
     x = layers.Flatten()(x)
     x = layers.Dense(256)(x)
     x = layers.LeakyReLU(0.2)(x)
     x = layers.Dropout(0.5)(x)
     x = layers.Dense(1)(x)
-    x = layers.Activation('sigmoid')
+    x = layers.Activation('sigmoid')(x)
 
     discriminator = keras.models.Model(discriminator_input, x)
     # discriminator.summary()
@@ -92,7 +92,6 @@ def main(args):
     save_dir = args.save_dir
 
     generator = build_generator()
-
     discriminator = build_discriminator()
     discriminator_optimizer = keras.optimizers.Adam(lr=0.0008, decay=1e-8)
     discriminator.compile(optimizer=discriminator_optimizer, loss='binary_crossentropy')
@@ -106,8 +105,7 @@ def main(args):
         random_latent_vectors = np.random.normal(size=(batch_size, latent_dim))
         random_labels = np.random.randint(0, CLASS_NUM, batch_size)
         random_labels_vectors = keras.utils.to_categorical(random_labels, CLASS_NUM)
-        random_vectors = np.concatenate([random_labels_vectors, random_labels_vectors], axis=1)
-
+        random_vectors = np.concatenate([random_latent_vectors, random_labels_vectors], axis=1)
         generated_iamges = generator.predict(random_vectors)
         random_labels_images = np.array([label2image(i) for i in random_labels])
         generated_concat_iamges = np.concatenate((generated_iamges, random_labels_images), axis=3)
@@ -131,13 +129,16 @@ def main(args):
 
         misleading_targets = np.zeros((batch_size, 1))
 
-        a_loss = gan.train_on_batch(random_latent_vectors, misleading_targets)
+        random_latent_vectors = np.random.normal(size=(batch_size, latent_dim))
+        random_labels = np.random.randint(0, CLASS_NUM, batch_size)
+        random_labels_vectors = keras.utils.to_categorical(random_labels, CLASS_NUM)
+        a_loss = gan.train_on_batch([random_latent_vectors, random_labels_vectors, random_labels_images], misleading_targets)
 
         start += batch_size
         if start > len(x_train) - batch_size:
             start = 0
 
-        if step % 1000 == 0:
+        if step % 1000 == 0 and step != 0:
             gan.save_weights('gan.h5')
 
             print('discriminator loss at step %s: %s' % (step, d_loss))
@@ -153,7 +154,7 @@ def main(args):
 def parse_arguments(argv):
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--iterations', type=int, default=10000)
+    parser.add_argument('--iterations', type=int, default=100000)
     parser.add_argument('--batch_size', type=int, default=20)
     parser.add_argument('--save_dir', type=str, default='./logs')
 
