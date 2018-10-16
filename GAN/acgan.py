5f0a30c8ecadb5a106f3bac89e41db1f41cf8ea5
ti-ginkgo <t_6_ginkgonut_18_i@yahoo.co.jp>
2018-10-16 18:11:19 +0900

    refactoring acfan

--- GAN/acgan.py
+++ GAN/acgan.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import argparse
+import pickle
 import numpy as np
 from tqdm import tqdm
 from collections import defaultdict
@@ -17,12 +18,11 @@ latent_dim = 100
 height = 28
 width = 28
 channels = 1
-CLASS_NUM = 10
 
 
 def build_generator(latent_dim):    
-    latent_input = keras.Input(shape=(latent_dim, ))
-    class_input = keras.Input(shape=(1,), dtype='int32')
+    latent_input = layers.Input(shape=(latent_dim, ))
+    class_input = layers.Input(shape=(1,), dtype='int32')
 
     emb = layers.Embedding(10, latent_dim, embeddings_initializer='glorot_normal')(class_input)
     emb = layers.Flatten()(emb)
@@ -75,17 +75,17 @@ def build_discriminator():
     fake = layers.Dense(1, activation='sigmoid', name='generation')(x)
     aux = layers.Dense(10, activation='softmax', name='auxiliary')(x)
 
-    return Model(input=discriminator_input, output=[fake, aux])
+    return keras.models.Model(input=discriminator_input, output=[fake, aux])
 
 
 def build_combined(latent_dim, generator, discriminator):
-    latent_input = Input(shape=(latent_dim, ))
-    class_input = Input(shape=(1,), dtype='int32')
+    latent_input = layers.Input(shape=(latent_dim, ))
+    class_input = layers.Input(shape=(1,), dtype='int32')
 
     fake_image = generator([latent_input, class_input])
 
     fake, aux = discriminator(fake_image)
-    combined = Model(input=[latent_input, class_input], output=[fake, aux])
+    combined = keras.models.Model(input=[latent_input, class_input], output=[fake, aux])
 
     return combined
 
@@ -94,6 +94,7 @@ def main(args):
     epochs = args.epochs
     batch_size = args.batch_size
     latent_dim = args.latent_dim
+    save_dir = args.save_dir
 
     discriminator = build_discriminator()
     discriminator.compile(
@@ -130,46 +131,35 @@ def main(args):
         print('Epoch {} of {}'.format(epoch + 1, epochs))
 
         nb_batches = int(x_train.shape[0] / batch_size)
-        progress_bar = utils.generic_utils.Progbar(target=nb_batches)
+        progress_bar = keras.utils.generic_utils.Progbar(target=nb_batches)
 
         epoch_gen_loss = []
         epoch_disc_loss = []
 
         for index in range(nb_batches):
             progress_bar.update(index)
-            # generate a new batch of noise
-            noise = np.random.uniform(-1, 1, (batch_size, latent_dim))
 
-            # get a batch of real images
+            noise = np.random.normal(size=(batch_size, latent_dim))
+            # noise = np.random.uniform(-1, 1, (batch_size, latent_dim))
+
             image_batch = x_train[index * batch_size:(index + 1) * batch_size]
             label_batch = y_train[index * batch_size:(index + 1) * batch_size]
 
-            # sample some labels from p_c
             sampled_labels = np.random.randint(0, 10, batch_size)
 
-            # generate a batch of fake images, using the generated labels as a
-            # conditioner. We reshape the sampled labels to be
-            # (batch_size, 1) so that we can feed them into the embedding
-            # layer as a length one sequence
             generated_images = generator.predict(
                 [noise, sampled_labels.reshape((-1, 1))], verbose=0)
 
-            X = np.concatenate((image_batch, generated_images))
+            x = np.concatenate((image_batch, generated_images))
             y = np.array([1] * batch_size + [0] * batch_size)
             aux_y = np.concatenate((label_batch, sampled_labels), axis=0)
 
-            # see if the discriminator can figure itself out...
-            epoch_disc_loss.append(discriminator.train_on_batch(X, [y, aux_y]))
+            epoch_disc_loss.append(discriminator.train_on_batch(x, [y, aux_y]))
 
-            # make new noise. we generate 2 * batch size here such that we have
-            # the generator optimize over an identical number of images as the
-            # discriminator
-            noise = np.random.uniform(-1, 1, (2 * batch_size, latent_dim))
+            noise = np.random.normal(size=(2 * batch_size, latent_dim))
+            # noise = np.random.uniform(-1, 1, (2 * batch_size, latent_dim))
             sampled_labels = np.random.randint(0, 10, 2 * batch_size)
 
-            # we want to train the genrator to trick the discriminator
-            # For the generator, we want all the {fake, not-fake} labels to say
-            # not-fake
             trick = np.ones(2 * batch_size)
 
             epoch_gen_loss.append(combined.train_on_batch(
@@ -177,28 +167,24 @@ def main(args):
 
         print('\nTesting for epoch {}:'.format(epoch + 1))
 
-        # evaluate the testing loss here
+        noise = np.random.normal(size=(nb_test, latent_dim))
+        # noise = np.random.uniform(-1, 1, (nb_test, latent_dim))
 
-        # generate a new batch of noise
-        noise = np.random.uniform(-1, 1, (nb_test, latent_dim))
-
-        # sample some labels from p_c and generate images from them
         sampled_labels = np.random.randint(0, 10, nb_test)
         generated_images = generator.predict(
             [noise, sampled_labels.reshape((-1, 1))], verbose=False)
 
-        X = np.concatenate((x_test, generated_images))
+        x = np.concatenate((x_test, generated_images))
         y = np.array([1] * nb_test + [0] * nb_test)
         aux_y = np.concatenate((y_test, sampled_labels), axis=0)
 
-        # see if the discriminator can figure itself out...
         discriminator_test_loss = discriminator.evaluate(
-            X, [y, aux_y], verbose=False)
+            x, [y, aux_y], verbose=False)
 
         discriminator_train_loss = np.mean(np.array(epoch_disc_loss), axis=0)
 
-        # make new noise
-        noise = np.random.uniform(-1, 1, (2 * nb_test, latent_dim))
+        noise = np.random.normal(size=(2 * nb_train, latent_dim))
+        # noise = np.random.uniform(-1, 1, (2 * nb_test, latent_dim))
         sampled_labels = np.random.randint(0, 10, 2 * nb_test)
 
         trick = np.ones(2 * nb_test)
@@ -209,7 +195,6 @@ def main(args):
 
         generator_train_loss = np.mean(np.array(epoch_gen_loss), axis=0)
 
-        # generate an epoch report on performance
         train_history['generator'].append(generator_train_loss)
         train_history['discriminator'].append(discriminator_train_loss)
 
@@ -230,33 +215,27 @@ def main(args):
         print(ROW_FMT.format('discriminator (test)',
                              *test_history['discriminator'][-1]))
 
-        # save weights every epoch
         generator.save_weights(
-            'params_generator_epoch_{0:03d}.hdf5'.format(epoch), True)
+            os.path.join(save_dir, 'params_generator_epoch_{0:03d}.hdf5'.format(epoch)), True)
         discriminator.save_weights(
-            'params_discriminator_epoch_{0:03d}.hdf5'.format(epoch), True)
+            os.path.join(save_dir, 'params_discriminator_epoch_{0:03d}.hdf5'.format(epoch)), True)
 
-        # generate some digits to display
-        noise = np.random.uniform(-1, 1, (100, latent_dim))
+        noise = np.random.normal(size=(100, latent_dim))
+        # noise = np.random.uniform(-1, 1, (100, latent_dim))
 
-        sampled_labels = np.array([
-            [i] * 10 for i in range(10)
-        ]).reshape(-1, 1)
+        sampled_labels = np.array([[i] * 10 for i in range(10)]).reshape(-1, 1)
 
-        # get a batch to display
-        generated_images = generator.predict(
-            [noise, sampled_labels], verbose=0)
+        generated_images = generator.predict([noise, sampled_labels], verbose=0)
 
-        # arrange them into a grid
         img = (np.concatenate([r.reshape(-1, 28)
                                for r in np.split(generated_images, 10)
                                ], axis=-1) * 127.5 + 127.5).astype(np.uint8)
 
         Image.fromarray(img).save(
-            'plot_epoch_{0:03d}_generated.png'.format(epoch))
+            os.path.join(save_dir, 'plot_epoch_{0:03d}_generated.png'.format(epoch)))
 
-    # pickle.dump({'train': train_history, 'test': test_history},
-    #             open('acgan-history.pkl', 'wb'))
+    pickle.dump({'train': train_history, 'test': test_history},
+                open('acgan-history.pkl', 'wb'))
 
 
 def parse_arguments(argv):
