161f5736f1f66618da7c34fc914dae3637a402d4
ti-ginkgo <ginkgonut.9960@gmail.com>
2018-11-03 16:43:27 +0900

    add new files

--- CNN/GazeEstimation/pytorch_model.py
+++ CNN/GazeEstimation/pytorch_model.py
@@ -1,9 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import torch.optim as optim
 import torchvision.models as models
-from torch.autograd import Variable
 
 
 class GazeNet(nn.Module):
@@ -12,18 +10,10 @@ class GazeNet(nn.Module):
         super(GazeNet, self).__init__()
         model = models.alexnet(pretrained=True)
         self.alexnet = model.features
-        self.layer1 = nn.Sequential(
-            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=1, stride=1, padding=0),
-            nn.ReLU()
-        )
-        self.layer2 = nn.Sequential(
-            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=1, stride=1, padding=0),
-            nn.ReLU()
-        )
-        self.layer3 = nn.Sequential(
-            nn.Conv2d(in_channels=256, out_channels=1, kernel_size=1, stride=1, padding=0),
-            nn.ReLU()
-        )
+
+        self.Conv1 = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=1, stride=1, padding=0)
+        self.Conv2 = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=1, stride=1, padding=0)
+        self.Conv3 = nn.Conv2d(in_channels=256, out_channels=1, kernel_size=1, stride=1, padding=0)
         self.layer4 = nn.Sequential(
             nn.Linear(256*13*13, 4096),
             nn.ReLU(),
@@ -36,15 +26,25 @@ class GazeNet(nn.Module):
         )   
         self.fc = nn.Linear(4096, 2)
 
+        self._initialize_weight()
+        self._initialize_bias()
+
+    def _initialize_weight(self):
+        nn.init.normal_(self.Conv1.weight, mean=0.0, std=0.01)
+        nn.init.normal_(self.Conv2.weight, mean=0.0, std=0.01)
+        nn.init.normal_(self.Conv3.weight, mean=0.0, std=0.001)
+
+    def _initialize_bias(self):
+        nn.init.constant_(self.Conv1.bias, val=0.1)
+        nn.init.constant_(self.Conv2.bias, val=0.1)
+        nn.init.constant_(self.Conv3.bias, val=1)
+
     def forward(self, x):
         x = self.alexnet(x)
 
-        y = self.layer1(x)
-        print(y.size())
-        y = self.layer2(y)
-        print(y.size())
-        y = self.layer3(y)
-        print(y.size())
+        y = F.relu(self.Conv1(x))
+        y = F.relu(self.Conv2(y))
+        y = F.relu(self.Conv3(y))
 
         x = F.dropout(F.relu(F.mul(x, y)), 0.5)
         x = x.view(x.size(0), -1)
@@ -53,25 +53,3 @@ class GazeNet(nn.Module):
         x = self.fc(x)
 
         return x
-
-
-# class Flatten(nn.Module):
-#     def forward(self, x):
-#         x = x.view(x.size(0), -1)
-#         return x
-
-# def weights_init(m):
-#     classname = m.__class__.__name__
-#     if classname.find('Conv') != -1:
-#         m.weight.data.normal_(0.0, 0.01)
-
-# class Conv2d(nn.Module):
-#     def __init__(self, in_channels, out_channels, kernel_size, stride, padding):
-#         self.in_channels = in_channels
-#         self.out_channels = out_channels
-#         self.kernel_size = kernel_size
-#         self.stride = stride
-#         self.padding = padding
-
-#         self.weight = nn.Parameter(torch.Tensor(out_channels, in_channels))
-#         self.bias = nn.Parameter(torch.Tensor(out_channels))
